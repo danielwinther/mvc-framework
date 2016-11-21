95be4ebd1b71e71a452a12c97380d65f7785b030
Daniel <danielwinther@hotmail.dk>
2016-11-21 02:20:36 +0100

    .

---
+++ application/config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* Initialize config variables
+*/
+function initializeConfig() {
+	return [
+		'defaultController' => 'Basic',
+		'defaultMethod' => 'index',
+		'defaultParameters' => [],
+		'defaultCharset' => 'UTF-8',
+	];
+}
---
+++ application/config/database.php
@@ -0,0 +1,13 @@
+<?php
+/**
+Initialize database variables
+*/
+function databaseConfig() {
+	return [
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'databaseName' => 'mvc-framework',
+		'prefix' => '',
+	];
+}
