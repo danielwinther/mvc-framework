db16e4ccd625f5b2984ae3e65645ad08c591b529
Daniel <danielwinther@hotmail.dk>
2017-01-19 11:41:11 +0100

    api

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,19 +4,13 @@ class Api extends BaseController {
 		$user = $this->loadModel('Users');
 		$user = Auth::user();
 
-		if ($user) {
-			$html = $this->scrape('GET', 'https://www.bedrebegravelse.dk/424/bedemand-priser-paa-begravelse-eller-bisaettelse');
-			$html = $html->find('#divPreview > .fourth');
-			
-			foreach ($html as $value) {
-				$products[] = ['typePrice' => $value->find('p', 0)->plaintext, 'description' => $value->find('a', 0)->title, 'image' => 'https://www.bedrebegravelse.dk/' . $value->find('img', 0)->src];
-			}
+		$html = $this->scrape('GET', 'https://www.bedrebegravelse.dk/424/bedemand-priser-paa-begravelse-eller-bisaettelse');
+		$html = $html->find('#divPreview > .fourth');
 
-			echo $this->convert($format, $products);
-		}
-		else {
-			Redirect::controller('Login@index');
+		foreach ($html as $value) {
+			$products[] = ['typePrice' => $value->find('p', 0)->plaintext, 'description' => $value->find('a', 0)->title, 'image' => 'https://www.bedrebegravelse.dk/' . $value->find('img', 0)->src];
 		}
 
+		echo $this->convert($format, $products);
 	}
 }
